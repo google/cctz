0518420bfcb7670abcf6f72089c200c3cb857433
Bradley White <bww@acm.org>
2016-03-11 16:57:31 -0500

    Simplify examples/example4.cc.

--- examples/example4.cc
+++ examples/example4.cc
@@ -21,8 +21,7 @@
 template <typename D>
 cctz::time_point<cctz::sys_seconds> FloorDay(cctz::time_point<D> tp,
                                              cctz::time_zone tz) {
-  const cctz::civil_second cs = cctz::convert(tp, tz);
-  const cctz::civil_day cd(cs.year(), cs.month(), cs.day());
+  const cctz::civil_day cd(cctz::convert(tp, tz));
   return cctz::convert(cctz::civil_second(cd), tz);
 }
 
