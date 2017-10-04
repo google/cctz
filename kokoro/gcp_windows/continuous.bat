7a4cee419db14797f57557366e13b833e3ee0216
Gennadiy Civil <gennadiycivil@users.noreply.github.com>
2017-10-04 14:35:53 -0400

    WIP

--- kokoro/gcp_windows/continuous.bat
+++ kokoro/gcp_windows/continuous.bat
@@ -1,6 +1,11 @@
+echo on
 set PATH=C:\Program Files\Java\jdk1.7.0_75\bin;%PATH%
 
+pwd
+
 cd %KOKORO_PIPER_DIR%\google3\experimental\users\misterg\cctz
+
 call build.bat
 
+echo off
 exit %ERRORLEVEL%
