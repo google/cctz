google/cctz

commit b820d31e80948a64ad68b88799759e098bc3a879

Link to CoreFoundation in time_zone (#102)
* link to CoreFoundation in TimeZone

* formatting

BUILD
@@ -14,6 +14,20 @@
 
 licenses(["notice"])  # Apache License
 
+config_setting(
+    name = "osx",
+    constraint_values = [
+        "@bazel_tools//platforms:osx"
+    ],
+)
+
+config_setting(
+    name = "ios",
+    constraint_values = [
+        "@bazel_tools//platforms:ios",
+    ],
+)
+
 ### libraries
 
 cc_library(
@@ -51,6 +65,15 @@ cc_library(
         "include/cctz/time_zone.h",
         "include/cctz/zone_info_source.h",
     ],
+    linkopts = select({
+        "//:osx": [
+            "-framework Foundation",
+        ],
+        "//:ios": [
+            "-framework Foundation",
+        ],
+        "//conditions:default": [],
+    }),
     includes = ["include"],
     visibility = ["//visibility:public"],
     deps = [":civil_time"],

commit 47e4d3fd03a0d59fb6408c91ce5bf24d724b2513

Correct some BUILD formatting errors introduced in #102.

BUILD
@@ -17,7 +17,7 @@ licenses(["notice"])  # Apache License
 config_setting(
     name = "osx",
     constraint_values = [
-        "@bazel_tools//platforms:osx"
+        "@bazel_tools//platforms:osx",
     ],
 )
 
@@ -65,6 +65,7 @@ cc_library(
         "include/cctz/time_zone.h",
         "include/cctz/zone_info_source.h",
     ],
+    includes = ["include"],
     linkopts = select({
         "//:osx": [
             "-framework Foundation",
@@ -74,7 +75,6 @@ cc_library(
         ],
         "//conditions:default": [],
     }),
-    includes = ["include"],
     visibility = ["//visibility:public"],
     deps = [":civil_time"],
 )